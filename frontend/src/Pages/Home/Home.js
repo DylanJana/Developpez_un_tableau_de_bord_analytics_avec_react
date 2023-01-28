a33da1bac9c0e5a10b9a0e16dc7ed9c34c3f18f8
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-28 18:49:40 +0100

    Create Home Page
    Create home with two users

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,8 +1,36 @@
+import { NavLink } from "react-router-dom";
+import karl from '../../assets/images/karl.webp';
+import cecilia from '../../assets/images/cecilia.webp';
+
+// Import css of Home.js
+import './Home.css';
+
 function Home() {
+
   return (
-    <div>
-      <h1>Page Home</h1>
-    </div>
+    <section className="l-container--md">
+      <div className="users-heading text--center">
+        <h1 className="title--xl">Bienvenue sur <span className="text--secondary">SportSee</span></h1>
+      </div>
+      <div className="users">
+        <div>
+          <NavLink to='user/12'>
+            <div className="users__pic mb--xs">
+              <img src={karl} alt="Profil de l'utilisateur" />
+            </div>
+            <p className="text--center title--sm">Je suis Karl</p> 
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="user/18"> 
+            <div className="users__pic">
+              <img src={cecilia} alt="Profil de l'utilisateur" />
+            </div>
+            <p className="text--center title--sm">Je suis Cecilia</p>
+          </NavLink>
+        </div>
+      </div>
+    </section>
   )
 }
 
